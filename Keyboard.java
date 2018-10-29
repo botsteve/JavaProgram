04adc51a68902eb37edde948d142c373abbc779c
botsteve <rusenstefan15@yahoo.com>
2018-10-29 23:56:12 +0200

    Hello scan

---
+++ Keyboard.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Keyboard{
+	public static void main(String [] args){
+		Scanner scan = new Scanner(System.in);
+		String s = scan.nextLine();
+		if(s.equals("Hi")){
+			System.out.println("Hello World");
+		}
+	}	
+}
